56076290448d62d53ab5af13ad010318422dcb32
Andrew Romanov <andrey.romanov@rambler-co.ru>
2016-11-14 09:42:28 +0300

    Add forms section

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,24 +1,30 @@
 const path = require('path');
 
 module.exports = {
- title: 'React UI kit boilerplate',
- components: './source/components/**/*.jsx',
- updateWebpackConfig(webpackConfig) {
-   const componentsDir = path.join(__dirname, 'source/components');
+	title: 'React UI kit boilerplate',
+	components: './source/components/**/*.jsx',
+	sections: [
+		{
+			name: 'Формы',
+			components: './source/components/*(Button|Input|Label|RadioToggle)/*.jsx',
+		},
+	],
+	updateWebpackConfig(webpackConfig) {
+		const componentsDir = path.join(__dirname, 'source/components');
 
-   webpackConfig.module.loaders.push(
-     {
-       test: /\.jsx?$/,
-       include: componentsDir,
-       loader: 'babel'
-     },
-     {
-       test: /\.styl$/,
-       include: componentsDir,
-       loader: 'style!css!stylus'
-     }
-   );
+		webpackConfig.module.loaders.push(
+			{
+				test: /\.jsx?$/,
+				include: componentsDir,
+				loader: 'babel',
+			},
+			{
+				test: /\.styl$/,
+				include: componentsDir,
+				loader: 'style!css!stylus',
+			}
+		);
 
-   return webpackConfig;
- },
+		return webpackConfig;
+	},
 };
